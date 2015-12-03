fe071b49949a86f5e6100558c58b1b30ec0f422a
Paul TREHIOU <paul.trehiou@utbm.fr>
2015-12-03 17:23:30 +0100

    Gestion des stocks

--- catan/src/com/catan/Joueur.java
+++ catan/src/com/catan/Joueur.java
@@ -1,6 +1,8 @@
 package com.catan;
 
+import java.util.ArrayList;
+
 public class Joueur {
     private String nom;
-
+    private ArrayList<StockRessource> stock;
 }
---
+++ catan/src/com/catan/StockRessource.java
@@ -0,0 +1,9 @@
+package com.catan;
+
+/**
+ * Created by paul on 03/12/15.
+ */
+public class StockRessource {
+    private Ressources type;
+    private int quantite;
+}
