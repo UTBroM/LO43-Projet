9a584736eed1f13906527199e50b4c5b24dc1ae9
nyanloutre <paul.trehiou@gmail.com>
2015-12-31 16:09:33 +0100

    Création d'un joueur de test

--- catan/src/com/catan/Plateau.java
+++ catan/src/com/catan/Plateau.java
@@ -3,6 +3,7 @@ package com.catan;
 import java.util.ArrayList;
 
 public class Plateau {
+    private ArrayList<Joueur> joueurs;
     private ArrayList<Case> cases;
     private ArrayList<Noeud> noeuds;
     private Voleur voleur;
@@ -10,6 +11,8 @@ public class Plateau {
     private Des des;
 
     public Plateau(){
-
+        joueurs = new ArrayList<Joueur>();
+        joueurs.add(new Joueur("test"));
+        System.out.println(joueurs.get(0).getStocRes(Ressources.METAL));
     }
 }
