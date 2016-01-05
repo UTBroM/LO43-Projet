383c3a0bbfa1221dd50b0b31846d2387761e90e8
Paul TREHIOU <paul.trehiou@utbm.fr>
2016-01-05 17:05:03 +0100

    Test avec 10 metals

--- catan/src/com/catan/Plateau.java
+++ catan/src/com/catan/Plateau.java
@@ -13,7 +13,8 @@ public class Plateau {
     public Plateau(){
         joueurs = new ArrayList<Joueur>();
         joueurs.add(new Joueur("test"));
-        System.out.println(joueurs.get(0).getStocRes(Ressources.METAL));
+        joueurs.get(0).setStockRes(Ressources.METAL, 10);
+        System.out.println("Ressources de metal : " + joueurs.get(0).getStocRes(Ressources.METAL));
         joueurs.get(0).creerDev();
         joueurs.get(0).setStockRes(Ressources.CHEESEBURGER, 10);
         joueurs.get(0).setStockRes(Ressources.LAINE, 10);
